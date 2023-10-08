8c41c16e4d8caa56d7534df39460cb6caf4172de
abhinavpurohit47 <purohitabhinav01@gmail.com>
2023-10-08 13:16:45 +0530

    ✨ Validation for fields

--- src/shipment/createShipment.dto.ts
+++ src/shipment/createShipment.dto.ts
@@ -1,12 +1,22 @@
+import { IsNotEmpty } from 'class-validator';
 export class CreateShipment {
+  @IsNotEmpty()
   ship_id: string;
+  @IsNotEmpty()
   ship_from_city: string;
+  @IsNotEmpty()
   ship_to_city: string;
+  @IsNotEmpty()
   ship_from_date: Date;
+  @IsNotEmpty()
   ship_to_date: Date;
+  @IsNotEmpty()
   weight_of_ship: number;
+  @IsNotEmpty()
   weight_unit: string;
+  @IsNotEmpty()
   expected_no_of_days: number;
+  @IsNotEmpty()
   ship_captain: string;
   ship_co_captain: string;
 }
--- src/shipment/getShipment.dto.ts
+++ src/shipment/getShipment.dto.ts
@@ -1,3 +1,5 @@
+import { IsNotEmpty } from 'class-validator';
 export class GetShipment {
+  @IsNotEmpty()
   guid: string;
 }
