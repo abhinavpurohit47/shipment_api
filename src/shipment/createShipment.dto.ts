d50af6e2e4737ef83bc4af570a30c4378c506873
abhinavpurohit47 <purohitabhinav01@gmail.com>
2023-10-08 13:11:11 +0530

    data transfer object defined

---
+++ src/shipment/createShipment.dto.ts
@@ -0,0 +1,12 @@
+export class CreateShipment {
+  ship_id: string;
+  ship_from_city: string;
+  ship_to_city: string;
+  ship_from_date: Date;
+  ship_to_date: Date;
+  weight_of_ship: number;
+  weight_unit: string;
+  expected_no_of_days: number;
+  ship_captain: string;
+  ship_co_captain: string;
+}
