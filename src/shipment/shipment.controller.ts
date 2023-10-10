d4fad5fbb61baa27f5ef75932068c4c71d338be7
abhinavpurohit47 <purohitabhinav01@gmail.com>
2023-10-10 11:09:11 +0530

    🔒️ business logic shifted to service from controller

--- src/shipment/shipment.controller.ts
+++ src/shipment/shipment.controller.ts
@@ -5,96 +5,43 @@ import {
   Post,
   Res,
   Param,
-  NotFoundException,
   Logger,
 } from '@nestjs/common';
-import { CreateShipment } from './createShipment.dto';
 import { ShipmentService } from './shipment.service';
 import { Response } from 'express';
-import { generateGuid } from 'src/utils/guid.util';
-import { CosmosClient } from '@azure/cosmos';
-import { Database, Container, ItemDefinition } from '@azure/cosmos';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
-const endpoint = process.env.DB_ENDPOINT;
-const key = process.env.DB_KEY;
-
-const databaseID = process.env.DB_ID;
-const containerID = process.env.CONTAINER_ID;
-console.log('Endpoint:', endpoint);
-console.log('Key:', key);
-console.log('DB_ID:', databaseID);
-console.log('Container: ', containerID);
-
-export const client = new CosmosClient({ endpoint, key });
+import { CreateShipment } from './createShipment.dto';
 
 @Controller('shipment')
 export class ShipmentController {
   private readonly logger = new Logger(ShipmentController.name);
-  private createdGuid: string;
-  shipmentService: ShipmentService;
-  private client: CosmosClient;
-  private database: Database;
-  private container: Container;
 
-  constructor(private readonly _shipmentService: ShipmentService) {
-    this.shipmentService = _shipmentService;
-    this.client = new CosmosClient({ endpoint, key });
-    this.database = this.client.database(databaseID);
-    this.container = this.database.container(containerID);
-  }
+  constructor(private readonly shipmentService: ShipmentService) {}
 
   @Get('/:guid')
   async getShipment(@Param('guid') guid: string, @Res() res: Response) {
-    if (!guid) {
-      throw new NotFoundException('GUID not provided');
-    }
-    this.logger.log(`Received GUID: ${guid}`);
     try {
-      const querySpec = {
-        query: 'SELECT * FROM c WHERE c.generated_guid = @guid',
-        parameters: [
-          {
-            name: '@guid',
-            value: guid,
-          },
-        ],
-      };
+      const shipment = await this.shipmentService.getShipmentByGuid(guid);
 
-      const { resources } = await this.container.items
-        .query(querySpec)
-        .fetchAll();
-      if (resources && resources.length > 0) {
-        this.logger.log(`Successfully retrieved shipment with GUID: ${guid}`);
-        return res.status(200).json(resources[0]);
-      } else {
-        this.logger.warn(
-          `Shipment with GUID ${guid} not found in the database`,
-        );
-        throw new NotFoundException('Shipment Not Found');
-      }
+      this.logger.log(`Successfully retrieved shipment with GUID: ${guid}`);
+      return res.status(200).json(shipment);
     } catch (error) {
-      this.logger.error(`Error retrieving shipment: ${error}`);
-      console.error('Error retrieving shipment: ', error);
-      throw new NotFoundException('Shipment Not Found');
+      this.logger.error(`Error retrieving shipment: ${error.message}`);
+      return res.status(error.status || 500).json({ error: error.message });
     }
   }
 
   @Post('/create')
   async createShipment(@Body() shipment: CreateShipment, @Res() res: any) {
-    const guid = generateGuid();
-    this.createdGuid = guid;
-    shipment.generated_guid = guid;
     try {
-      const { resource } = await this.container.items.create(
-        shipment as ItemDefinition,
+      const createdShipment =
+        await this.shipmentService.createShipment(shipment);
+      this.logger.log(
+        `Successfully Posted the shipment Details with GUID as: ${createdShipment.generated_guid}`,
       );
-      res.status(201).json({ guid });
-      return resource;
+      res.status(201).json({ guid: createdShipment.generated_guid });
     } catch (error) {
-      console.log('Error creating shipment: ', error);
-      return res.status(500).json({ error: 'Internal Server Error' });
+      this.logger.error(`Error creating shipment: ${error.message}`);
+      return res.status(error.status || 500).json({ error: error.message });
     }
   }
 }
--- src/shipment/shipment.service.ts
+++ src/shipment/shipment.service.ts
@@ -1,12 +1,73 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  CosmosClient,
+  Container,
+  Database,
+  ItemDefinition,
+} from '@azure/cosmos';
 import { CreateShipment } from './createShipment.dto';
+import { generateGuid } from 'src/utils/guid.util';
+import { configDotenv } from 'dotenv';
+
+configDotenv();
+@Injectable()
 export class ShipmentService {
-  getShipment(guid: string): Promise<CreateShipment> {
-    return null;
+  private readonly client: CosmosClient;
+  private readonly database: Database;
+  private readonly container: Container;
+
+  constructor() {
+    const endpoint = process.env.DB_ENDPOINT;
+    const key = process.env.DB_KEY;
+    const databaseID = process.env.DB_ID;
+    const containerID = process.env.CONTAINER_ID;
+
+    this.client = new CosmosClient({ endpoint, key });
+    this.database = this.client.database(databaseID);
+    this.container = this.database.container(containerID);
   }
 
-  createShipment(shipment: CreateShipment): Promise<string> {
-    return null;
+  async getShipmentByGuid(guid: string) {
+    if (!guid) {
+      throw new NotFoundException('GUID not provided');
+    }
+
+    try {
+      const querySpec = {
+        query: 'SELECT * FROM c WHERE c.generated_guid = @guid',
+        parameters: [
+          {
+            name: '@guid',
+            value: guid,
+          },
+        ],
+      };
+
+      const { resources } = await this.container.items
+        .query(querySpec)
+        .fetchAll();
+
+      if (resources && resources.length > 0) {
+        return resources[0];
+      } else {
+        throw new NotFoundException('Shipment Not Found');
+      }
+    } catch (error) {
+      throw new NotFoundException('Shipment Not Found');
+    }
+  }
+
+  async createShipment(shipment: CreateShipment) {
+    const guid = generateGuid(); // Assuming you have a function to generate a GUID
+    shipment.generated_guid = guid;
+
+    try {
+      const { resource } = await this.container.items.create(
+        shipment as ItemDefinition,
+      );
+      return resource;
+    } catch (error) {
+      throw new Error('Internal Server Error');
+    }
   }
 }
