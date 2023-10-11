b308416f8ea1a9e0ce9e4b49b69874d9aef11959
abhinavpurohit47 <purohitabhinav01@gmail.com>
2023-10-11 09:20:08 +0530

    🥅 City Validation Done

--- src/shipment/createShipment.dto.ts
+++ src/shipment/createShipment.dto.ts
@@ -1,23 +1,37 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsNumber, Min } from 'class-validator';
+import { Match } from './match.decorator';
 export class CreateShipment {
   generated_guid: string;
+
   @IsNotEmpty()
   ship_id: string;
+
   @IsNotEmpty()
   ship_from_city: string;
+
   @IsNotEmpty()
+  @Match('ship_to_city')
   ship_to_city: string;
+
   @IsNotEmpty()
   ship_from_date: Date;
+
   @IsNotEmpty()
   ship_to_date: Date;
+
   @IsNotEmpty()
   weight_of_ship: number;
+
   @IsNotEmpty()
   weight_unit: string;
+
   @IsNotEmpty()
+  @IsNumber()
+  @Min(1)
   expected_no_of_days: number;
+
   @IsNotEmpty()
   ship_captain: string;
+
   ship_co_captain: string;
 }
